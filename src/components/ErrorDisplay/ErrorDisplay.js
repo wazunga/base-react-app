d724f1a5c090d37326d8bd25e52d63cd1fe49eca
ismaelo <ismaelo@pop-os.localdomain>
2021-04-22 08:03:18 -0600

    component ErrorDisplay

---
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ErrorDisplay = ({img, title, message, background="bg-white"}) => (
+	<header className= {` ${background} dark:bg-gray-800`}>
+        <div className="container px-6 py-16 mx-auto">
+            <div className="items-center md:flex">
+	
+				<div className="flex items-center justify-center w-full mt-6 md:mt-0 md:w-1/2 md:order-2 ">
+                    <img className="w-full h-full max-w-2xl" src={img}/>
+                </div>
+
+				<div className="w-full md:w-1/2 md:order-1">
+                    <div className="max-w-lg">
+                        <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-white md:text-3xl">{title}</h1>
+                        <p className="mt-2 text-gray-600 dark:text-gray-400">{message}</p>
+                        <button className="px-3 py-2 mt-4 mx-2 text-sm font-medium text-white uppercase bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Shop Now</button>
+						<button className="px-3 py-2 mt-4 mx-2 text-sm font-medium text-white uppercase bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Shop Now</button>
+
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+    </header>				
+);
+
+export default ErrorDisplay;
+
--- src/pages/SiteStatus/index.js
+++ src/pages/SiteStatus/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import ErrorDisplay from '../../components/ErrorDisplay/ErrorDisplay'
+import errorTest from '../../assets/testErrorDisplay.svg'
 
 const SiteStatus = () => (
-  <div><h1>SiteStatus</h1></div>
+	<Fragment>
+		<div><h1>SiteStatus</h1></div>
+		<ErrorDisplay
+			img={errorTest}
+			title="Title Test"
+			message="Message Test"
+		/>
+	</Fragment>
+ 
 )
 
 export default SiteStatus
