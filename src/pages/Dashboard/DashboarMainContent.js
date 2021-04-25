7e6d7f3923115fc9b217cb8afa060056c6685606
Sergio Bernal <sergiobernal909@gmail.com>
2021-04-24 22:13:29 -0600

    routing on dashboard

--- src/pages/Dashboard/DashboarMainContent.js
+++ src/pages/Dashboard/DashboarMainContent.js
@@ -1,27 +1,7 @@
 import React from 'react'
 
-export const DashboardMainContent = () => (
+export const DashboardMainContent = ({ children }) => (
   <main class="m-4">
-    <p class="text-base font-medium">
-      See full project with all finished pages
-    </p>
-    <div class="space-x-4">
-      <a
-        href="https://kamona-wd.github.io/kwd-dashboard/"
-        target="_blank"
-        rel="noreferrer"
-        class="inline-block text-blue-600 hover:underline"
-      >
-        Live
-      </a>
-      <a
-        href="https://github.com/Kamona-WD/kwd-dashboard"
-        target="_blank"
-        rel="noreferrer"
-        class="inline-block text-blue-600 hover:underline"
-      >
-        Github repo
-      </a>
-    </div>
+    {children}
   </main>
 )
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,3 +1,4 @@
+import { Router } from '@reach/router'
 import React from 'react'
 import { DashboardNav } from '../../components/DashboardNav'
 import { Sidebar } from '../../components/Sidebar'
@@ -5,14 +6,24 @@ import { DashboardContainer } from './DashboardContainer'
 import { DashboardContent } from './DashboardContent'
 import { DashboardMainContent } from './DashboarMainContent'
 
-const Dashboard = () => (
+const Section = ({ title }) => (
+  <div className="bg-gray-50">
+    <h1 className="text-2xl">{title}</h1>
+  </div>
+)
+
+export const Dashboard = () => (
   <DashboardContainer>
     <Sidebar />
     <DashboardContent>
       <DashboardNav />
-      <DashboardMainContent />
+      <DashboardMainContent>
+        <Router>
+          <Section title="Dashboard Home" path="/" />
+          <Section title="Link 1" path="link1" />
+          <Section title="Link 2" path="link2" />
+        </Router>
+      </DashboardMainContent>
     </DashboardContent>
   </DashboardContainer>
 )
-
-export default Dashboard
