8ea90b7682a7aa77e0cfaf6e01d4ef206c81ad9d
Sergio Bernal <sergiobernal909@gmail.com>
2021-04-24 21:40:23 -0600

    optimizing component

--- src/components/DashboardNav/Buttons.js
+++ src/components/DashboardNav/Buttons.js
@@ -3,32 +3,19 @@ import { BellIcon, SearchIcon, CogIcon } from '@heroicons/react/outline'
 import { Avatar } from './Avatar'
 
 export const Buttons = () => {
-  const NotificationButton = () => (
-    <button className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800">
-      <span class="sr-only">Open Notification panel</span>
-      <BellIcon className="w-7 h-7" />
-    </button>
-  )
-
-  const SearchButton = () => (
-    <button className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800">
-      <span class="sr-only">Open Notification panel</span>
-      <SearchIcon className="w-7 h-7" />
-    </button>
-  )
-
-  const SettingsButton = () => (
-    <button className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800">
-      <span class="sr-only">Open Notification panel</span>
-      <CogIcon className="w-7 h-7" />
+  const Button = ({ Icon, sr }) => (
+    <button
+      className="p-2 text-blue-400 transition-colors duration-200 rounded-full bg-blue-50 hover:text-blue-600 hover:bg-blue-100 dark:hover:text-light dark:hover:bg-blue-700 dark:bg-dark focus:outline-none focus:bg-blue-100 dark:focus:bg-blue-700 focus:ring-blue-800">
+      <span className="sr-only">{sr}</span>
+      <Icon className="w-7 h-7" />
     </button>
   )
 
   return (
     <nav className="hidden space-x-2 md:flex md:items-center">
-      <NotificationButton />
-      <SearchButton />
-      <SettingsButton />
+      <Button Icon={BellIcon} sr="Notification"/>
+      <Button Icon={SearchIcon} sr="Search"/>
+      <Button Icon={CogIcon} sr="Settings"/>
       <Avatar />
     </nav>
   )
