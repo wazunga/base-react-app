d83a47647db0491a405163fa99ebaec4e4abfe6f
Sergio Bernal <sergiobernal909@gmail.com>
2021-04-26 16:27:04 -0600

    adding useDropdown

---
+++ src/hooks/useDropdown.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [openClasses, setOpenClasses] = useState('')
+
+  const handleIsOpen = (e) => {
+    e.preventDefault()
+    if (isOpen) {
+      setIsOpen(false)
+      setOpenClasses('')
+    } else if(!isOpen) {
+      setIsOpen(true)
+      setOpenClasses('bg-blue-100 dark:bg-blue-600')
+    }
+  }
+
+  return [isOpen, handleIsOpen, openClasses]
+}
