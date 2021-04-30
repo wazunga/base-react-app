9f545e2a69a176fd423997228daaf085265652ba
Sergio Bernal <sergiobernal909@gmail.com>
2021-04-29 22:21:05 -0600

    converting modal into components

---
+++ src/components/Modal/ModalIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ExclamationCircleIcon } from '@heroicons/react/outline'
+
+export const ModalIcon = ({ type }) => (
+  <div className="flex items-center justify-center flex-shrink-1 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+    <ExclamationCircleIcon className="w-7 h-6 text-red-600" />
+  </div>
+
+)
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,50 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ExclamationCircleIcon } from '@heroicons/react/outline'
+import { ModalContainer } from './ModalContainer'
+import { ModalInfo } from './ModalInfo'
+import { ModalIcon } from './ModalIcon'
+import { ModalDetail } from './ModalDetail'
+import { ModalButtons } from './ModalButtons'
+import { ModalAction } from './ModalAction'
+import { ModalCancel } from './ModalCancel'
 
-export const Modal = ({ title, content, onClose, onAction }) => {
+export const Modal = (props) => {
   return (
-    <div className="fixed inset-0 z-10 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
-
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-
-        <div className="inline-block overflow-hidden text-left align-bottom bg-white rounded-lg shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
-                <ExclamationCircleIcon className="w-6 h-6 text-red-600"/>
-              </div>
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 className="text-lg font-medium text-gray-900 leading-6" id="modal-title">
-                  {title}
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">{content}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button 
-              type="button" 
-              className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={onAction}
-            >
-              Deactivate
-            </button>
-            <button 
-              type="button" 
-              className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={onClose}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ModalContainer>
+      <ModalInfo>
+        <ModalIcon {...props} />
+        <ModalDetail {...props} />
+      </ModalInfo>
+      <ModalButtons>
+        <ModalAction {...props}>Aceptar</ModalAction>
+        <ModalCancel {...props} />
+      </ModalButtons >
+    </ModalContainer >
   )
 }
 
@@ -53,5 +28,6 @@ Modal.propTypes = {
   content: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
   onAction: PropTypes.func.isRequired,
-  isOpen: PropTypes.bool.isRequired
+  isOpen: PropTypes.bool.isRequired,
+  type: PropTypes.oneOf('confirm', 'delete')
 }
