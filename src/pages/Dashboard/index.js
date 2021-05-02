12b274c65b58784f4243e8641176326bc2e60a55
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-01 23:23:33 -0600

    refactoring filename

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,7 +5,7 @@ import { DashboardNav } from '../../components/DashboardNav'
 import { DashboardSidebar } from '../../components/DashboardSidebar'
 import { DashboardContainer } from './DashboardContainer'
 import { DashboardContent } from './DashboardContent'
-import { DashboardMainContent } from './DashboarMainContent'
+import { DashboardMainContent } from './DashboardMainContent'
 import { Test } from './DashboardPages/Test'
 import { Users } from './DashboardPages/Users'
 
