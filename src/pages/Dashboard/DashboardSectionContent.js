ec955979d63e596475317637a60673d84e4ea376
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-01 23:20:58 -0600

    assing section (model) layout

---
+++ src/pages/Dashboard/DashboardSectionContent.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const DashboardSectionContent = ({ children }) => (
+  <div className="flex flex-col p-1 pt-3">{children}</div>
+)
---
+++ src/pages/Dashboard/DashboardSectionTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const DashboardSectionTitle = ({ title }) => (
+  <div className="flex pt-1 pr-1 mb-3 justify-content">
+    <h1 className="text-2xl">{title}</h1>
+  </div>
+)
+
+DashboardSectionTitle.propTypes = {
+  title: PropTypes.string.isRequired
+}
