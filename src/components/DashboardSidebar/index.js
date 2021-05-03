5b0daa03564607e5c92447da22f34c357bf581ed
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-03 15:30:59 -0600

    Handling menu with DashboardMenu component

--- src/components/DashboardSidebar/index.js
+++ src/components/DashboardSidebar/index.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import { Sidenav } from './Sidenav'
-import { SidenavLinks } from './SidenavLinks'
 import { SidenavLinksContainer } from './SidenavLinksContainer'
-
-const links = [
-  { path: '/', title: 'action 1' },
-  { path: '/', title: 'action 2' }
-]
-
-const userLinks = [
-  { path: './users', title: 'Ver usuarios' },
-  { path: './users/create', title: 'Crear usuarios' },
-  { path: './users/edit', title: 'Editar usuarios' },
-  { path: './users/delete', title: 'Borrar usuarios' }
-]
+import { DashboardMenu } from '../DashboardMenu'
 
 export const DashboardSidebar = () => (
   <Sidenav>
     <SidenavLinksContainer>
-      <SidenavLinks title="Usuarios" path="users" links={userLinks} />
-      <SidenavLinks title="link 2" path="link2" links={links} />
+      <DashboardMenu />
     </SidenavLinksContainer>
   </Sidenav>
 )
