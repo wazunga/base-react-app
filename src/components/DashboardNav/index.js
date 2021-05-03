0e330a5e89d55266e4e138f1f38995b19e2a5ad6
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-03 14:32:32 -0600

    adding mobile menu button

--- src/components/DashboardNav/index.js
+++ src/components/DashboardNav/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Brand } from './Brand'
 import { Buttons } from './Buttons'
 import { Nav } from './Nav'
+import { MobileMenuButton } from './MobileMenuButton'
 
 export const DashboardNav = () => (
   <Nav>
+    <MobileMenuButton />
     <Brand title="base-react-app" />
     <Buttons />
   </Nav>
