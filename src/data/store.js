4cc987e7effd1d93a8c9db530a993e7e6c458a77
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-13 22:56:37 -0600

    adding store file

---
+++ src/data/store.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    user_name: 'Usuario 1',
+    user_username: 'Username',
+    user_role: 'Admin',
+    user_status: 'Active'
+  },
+  {
+    user_name: 'Usuario 2',
+    user_username: 'Username',
+    user_role: 'Admin',
+    user_status: 'Inactive'
+  },
+  {
+    user_name: 'Usuario 3',
+    user_username: 'Username',
+    user_role: 'Admin',
+    user_status: 'Banned'
+  },
+]
