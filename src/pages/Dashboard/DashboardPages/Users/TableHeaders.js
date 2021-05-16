5440c5eacfba2c04dbc471ea7cda18227fa3844a
Sergio Bernal <sergiobernal909@gmail.com>
2021-05-15 21:22:07 -0600

    adding table headers

---
+++ src/pages/Dashboard/DashboardPages/Users/TableHeaders.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TableHeaders = ({ headers }) => (
+  <thead>
+    <tr className="text-sm leading-normal text-gray-600 uppercase bg-gray-200">
+      {headers.map((header, key) => {
+        if (key < 1)
+          return <th className="px-6 py-3 text-left">{header}</th>
+            return <th className="px-6 py-3 text-center">{header}</th>
+      })}
+    </tr>
+  </thead>
+)
